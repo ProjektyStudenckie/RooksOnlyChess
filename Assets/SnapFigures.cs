591e8e144da401c37605b18887cb6021cb6ef0d8
Jakub Sosna <jakub.sosna@icloud.com>
2020-10-10 23:33:26 +0200

    Add rooks and order the blocks

---
+++ Assets/SnapFigures.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapFigures : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snap;
+        snap.x = Mathf.RoundToInt(transform.position.x - .5f);
+        snap.z = Mathf.RoundToInt(transform.position.z - .5f);
+
+        transform.position = new Vector3(snap.x + .5f, 0.85f, snap.z + .5f);
+    }
+}
