f15cfb28f7dc0e262c499b867c617432a0ab6c0e
Shpaner <Shpaner@users.noreply.github.com>
2020-10-19 21:23:28 +0200

    add chess board

--- Assets/Scripts/SnapBlocks.cs
+++ Assets/Scripts/SnapBlocks.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class Snap : MonoBehaviour
+public class SnapBlocks : MonoBehaviour
 {
-
-    // Update is called once per frame
     void Update()
     {
         Vector3 snap;
         snap.x = Mathf.RoundToInt(transform.position.x - .5f);
         snap.z = Mathf.RoundToInt(transform.position.z - .5f);
 
-        transform.position = new Vector3(snap.x + .5f, 0f, snap.z + 0.5f);
+        transform.position = new Vector3(snap.x + .5f, -0.51f, snap.z + 0.5f);
     }
 }
